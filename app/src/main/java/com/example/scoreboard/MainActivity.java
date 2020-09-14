f1b321250f896c1631647182b95dd023c33d0d79
pteacher <subfear@mail.ru>
2020-09-14 10:17:55 +0600

    Second commit

---
+++ app/src/main/java/com/example/scoreboard/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.scoreboard;
+
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
